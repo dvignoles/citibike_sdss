9b53c76f6181a8448b6c9a63643b5cab204111dc
Daniel Vignoles <dvignoles@gmail.com>
2023-02-26 20:26:19 -0500

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A spatial description support system for NYC Citi Bike dock placement & expansion',
+    author='Daniel Vignoles',
+    license='MIT',
+)
