f267f4dcf6bdd7dab70e764b1cc77385ffcb8c62
Ian Grant <ian.conway.grant@gmail.com>
2023-03-19 17:54:05 -0400

    Added docstrings.

--- src/GPKG_pipeline/GPKG_pipeline.py
+++ src/GPKG_pipeline/GPKG_pipeline.py
@@ -4,7 +4,7 @@ import warnings
 
 # Get GeoJSON from URL
 def json_response(url, limit=500000):
-    # Limit will determine how many rows are read
+    """Returns JSON response for the url with the specified limit parameter."""
     my_params = {"$limit": limit}
     with requests.get(url, params=my_params) as response:
         return response.json()
@@ -12,6 +12,14 @@ def json_response(url, limit=500000):
 
 # Create GeoDataFrame from URL
 def gdf_from_url(url, limit=500000):
+    """Requests the data from url and returns a GeoDataFrame.
+
+    Arguments:
+    url - The URL to which the request will be made. Should return a GeoJSON of type FeatureCollection.
+    limit - The limit parameter for the request, indicating how many records will be requested.
+
+    Returns:
+    gdf - A GeoDataFrame with the data from url."""
     response = json_response(url, limit)
     gdf = gp.GeoDataFrame.from_features(response)
     return gdf
@@ -19,6 +27,19 @@ def gdf_from_url(url, limit=500000):
 
 # Create a dict of gdfs matching a dict of URLS
 def gdf_dict(url_dict, limit=500000):
+    """Creates a dict of GeoDataFrames from a dict of urls.
+
+    Arguments:
+    url_dict - A dict of the form {name : url}, where name is the name
+    that the user will use to refer to the data from the URL. limit -
+    The limit parameter for the request, indicating how many records
+    will be requested.
+
+    Returns:
+    gdf_dict - A dict of the form {name : GeoDataFrame}, where the
+    urls from url_dict are replaced with the corresponding
+    GeoDataFrames.
+    """
     ans = {}
     for key in url_dict:
         ans[key] = gdf_from_url(url_dict[key], limit)
@@ -34,6 +55,11 @@ def gdf_dict(url_dict, limit=500000):
 
 # Export dict of GeoDataFrames to a GeoPackage
 def gdf_dict_to_gpkg(gdf_dict, path):
+    """Writes a dict of GeoDataFrames as the layers of a GeoPackage.
+
+    Arguments:
+    gdf_dict - a dict of GDFs in the format returned by gdf_dict().
+    path - the path to which the GeoPackage will be written."""
     for key in gdf_dict:
         gdf_dict[key].to_file(path, layer=key, driver="GPKG")
 
@@ -41,6 +67,18 @@ def gdf_dict_to_gpkg(gdf_dict, path):
 # Produce a GeoPackage from a dict of URLs
 # Return the dict of GeoDataFrames produced during processing
 def urls_to_gpkg(url_dict, path, max_size=500000):
+    """Writes a GeoPackage with the data from a dict of URLs.
+
+    Arguments:
+    url_dict - A dict of the form {name : url}, where name is the name
+    that the user will use to refer to the data from the URL.
+    path - The path to which the GeoPackage will be written.
+
+    Returns: 
+    gdf_dict - A dict of the form {name : GeoDataFrame}, where the
+    urls from url_dict are replaced with the corresponding
+    GeoDataFrames.
+"""
     print("Creating dict of GeoDataFrames...")
     my_gdf_dict = gdf_dict(url_dict, max_size)
     print("GeoDataFrame dict complete.")
