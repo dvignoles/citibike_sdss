31b0ec9cf898a4e36e9a4b89f3df3ed21d177292
Daniel Vignoles <dvignoles@gmail.com>
2023-05-08 00:00:50 -0400

    add peak summary tables

--- src/data/mta.py
+++ src/data/mta.py
@@ -101,6 +101,7 @@ class MtaTurnstiles(util.Source):
                 "exits bigint NOT NULL, "
                 "net_entries bigint, "
                 "net_exits bigint, "
+                "hours_difference float,"
                 "filename varchar NOT NULL "
                 ")"
             )
@@ -123,6 +124,7 @@ class MtaTurnstiles(util.Source):
                     net_sum AS (
                     SELECT
                     id,
+                    hours_difference,
                     CASE WHEN abs(calculated_net_entries) < 10000 AND hours_difference <= 24
                         THEN abs(calculated_net_entries) ELSE NULL END as net_entries,
                     CASE WHEN abs(calculated_net_exits) < 10000 AND hours_difference <= 24
@@ -131,6 +133,7 @@ class MtaTurnstiles(util.Source):
                     )
                     UPDATE turnstile_observations
                     SET
+                    hours_difference = (SELECT hours_difference FROM net_sum WHERE net_sum.id = turnstile_observations.id),
                     net_entries = (SELECT CAST(net_entries as Integer) FROM net_sum WHERE net_sum.id = turnstile_observations.id),
                     net_exits = (SELECT CAST(net_exits as Integer) FROM net_sum WHERE net_sum.id = turnstile_observations.id)
                     WHERE id in (SELECT id from net_sum)
@@ -168,6 +171,66 @@ class MtaTurnstiles(util.Source):
         con.execute(update_sql)
         con.commit()
 
+    def _update_daily_period_subunit(self, con, period):
+        """Create daily summary table per unit for specified peak/off-peak period"""
+
+        assert period in ("morning_peak", "evening_peak", "peak", "offpeak")
+
+        match period:
+            case "morning_peak":
+                where = "WHERE starts.period_start_hour IN (4,5,6,7) AND starts.weekday IN (1,2,3,4,5)"
+                table = "daily_morning_peak_subunit"
+            case "evening_peak":
+                where = "WHERE starts.period_start_hour IN (14,15,16,17) AND starts.weekday IN (1,2,3,4,5)"
+                table = "daily_evening_peak_subunit"
+            case "peak":
+                where = "WHERE starts.period_start_hour IN (4,5,6,7,14,15,16,17) AND starts.weekday IN (1,2,3,4,5)"
+                table = "daily_peak_subunit"
+            case "offpeak":
+                where = "WHERE starts.period_start_hour NOT IN (4,5,6,7,14,15,16,17) OR starts.weekday IN (0,6)"
+                table = "daily_offpeak_subunit"
+
+        # create initial table
+        con.execute(f"DROP TABLE IF EXISTS {table}")
+        create_sql = f"""
+                CREATE TABLE {table} AS
+                    WITH starts AS (
+                        SELECT
+                        id,
+                        unit_id,
+                        observed_at,
+                        hours_difference,
+                        net_entries,
+                        net_exits,
+                        CAST(strftime('%H', DATETIME(observed_at, '-4 hour')) AS bigint) as period_start_hour,
+                        CAST(strftime('%w', observed_at) AS bigint) as weekday
+                        FROM turnstile_observations
+                        WHERE hours_difference < 5 AND hours_difference > 3
+                    )
+                    SELECT
+                    unit_id,
+                    date(datetime(observed_at, '-2 hour', 'start of day')) as date,
+                    SUM(net_entries) AS entries,
+                    SUM(net_exits) AS exits,
+                    NULL as remoteunit
+                    FROM starts
+                    {where}
+                    GROUP BY unit_id, datetime(observed_at, '-2 hour', 'start of day')
+            """
+        con.execute(create_sql)
+        con.commit()
+
+        # fill in remoteunit
+        update_sql = f"""
+        WITH ru as(
+            SELECT DISTINCT unit_id, remoteunit FROM turnstile_observations
+        )
+        UPDATE {table}
+        SET remoteunit = (SELECT remoteunit FROM ru WHERE ru.unit_id = {table}.unit_id)
+        """
+        con.execute(update_sql)
+        con.commit()
+
     def _update_daily_complex(self, con):
         """Create daily counts per complex"""
 
@@ -201,6 +264,115 @@ class MtaTurnstiles(util.Source):
             GROUP BY complex_id, date
             """
         con.execute(create_sql)
+        con.commit()
+
+    def _update_daily_period_complex(self, con, period):
+        """Create daily counts per complex"""
+
+        assert period in ("morning_peak", "evening_peak", "peak", "offpeak")
+
+        match period:
+            case "morning_peak":
+                subunit_table = "daily_morning_peak_subunit"
+                table = "daily_morning_peak_complex"
+            case "evening_peak":
+                subunit_table = "daily_evening_peak_subunit"
+                table = "daily_evening_peak_complex"
+            case "peak":
+                subunit_table = "daily_peak_subunit"
+                table = "daily_peak_complex"
+            case "offpeak":
+                subunit_table = "daily_offpeak_subunit"
+                table = "daily_offpeak_complex"
+
+        con.execute(f"DROP TABLE IF EXISTS {table}")
+
+        create_sql = f"""
+            CREATE TABLE {table} AS
+            WITH unique_remotes AS(
+                SELECT DISTINCT complex_id, remote FROM remote_complex_lookup
+            ),
+            daily_sum AS(
+                SELECT
+                    CASE
+                    WHEN unique_remotes.complex_id IS NULL
+                    THEN remoteunit
+                    ELSE unique_remotes.complex_id
+                    END as complex_id,
+                    remoteunit,
+                    date,
+                    entries,
+                    exits
+                FROM {subunit_table}
+                LEFT JOIN unique_remotes
+                ON {subunit_table}.remoteunit = unique_remotes.remote
+            )
+            SELECT
+            complex_id,
+            date,
+            sum(entries) as entries,
+            sum(exits) as exits
+            FROM daily_sum
+            GROUP BY complex_id, date
+            """
+        con.execute(create_sql)
+        con.commit()
+
+    def _update_monthly_yearly_complex(self, con, period=None):
+        assert (
+            period in ("morning_peak", "evening_peak", "peak", "offpeak")
+            or period is None
+        )
+
+        match period:
+            case None:
+                table = "complex"
+            case "morning_peak":
+                table = "morning_peak_complex"
+            case "evening_peak":
+                table = "evening_peak_complex"
+            case "peak":
+                table = "peak_complex"
+            case "offpeak":
+                table = "offpeak_complex"
+
+        con.execute(f"DROP TABLE IF EXISTS monthly_{table}")
+        con.execute(f"DROP TABLE IF EXISTS annual_{table}")
+
+        monthly_sql = f"""
+            CREATE TABLE monthly_{table} AS
+            SELECT
+            complex_id,
+            CAST(strftime("%Y", date) as bigint) as year,
+            CAST(strftime("%m", date) as bigint) as month,
+            CAST(AVG(entries) as bigint) as mean_daily_entries,
+            SUM(entries) as total_entries,
+            CAST(AVG(exits) as bigint) as mean_daily_exits,
+            SUM(exits) as total_exits
+            FROM
+            daily_{table}
+            GROUP BY complex_id, strftime("%m-%Y", date)
+            HAVING COUNT(*) > 10
+            ORDER BY complex_id, year, month
+        """
+
+        annual_sql = f"""
+            CREATE TABLE annual_{table} AS
+            SELECT
+            complex_id,
+            CAST(strftime("%Y", date) as bigint) as year,
+            CAST(AVG(entries) as bigint) as mean_daily_entries,
+            SUM(entries) as total_entries,
+            CAST(AVG(exits) as bigint) as mean_daily_exits,
+            SUM(exits) as total_exits
+            FROM
+            daily_{table}
+            GROUP BY complex_id, strftime("%Y", date)
+            ORDER BY complex_id, year
+        """
+
+        con.execute(monthly_sql)
+        con.execute(annual_sql)
         con.commit()
 
     def raw_to_gpkg(
@@ -258,6 +430,7 @@ class MtaTurnstiles(util.Source):
                             station, linenames, division, date, time, observed_at, description, entries, exits,
                             CAST(NULL as bigint) as net_entries,
                             CAST(NULL as bigint) as net_exits,
+                            CAST(NULL as float) as hours_difference,
                             filename
                             FROM {temp_table}
                         )
@@ -271,3 +444,9 @@ class MtaTurnstiles(util.Source):
             self._update_net_values(con)
             self._update_daily_subunit(con)
             self._update_daily_complex(con)
+            self._update_monthly_yearly_complex(con)
+
+            for period in ("morning_peak", "evening_peak", "peak", "offpeak"):
+                self._update_daily_period_subunit(con, period)
+                self._update_daily_period_complex(con, period)
+                self._update_monthly_yearly_complex(con, period)
